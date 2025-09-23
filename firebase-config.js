54a07a9f9b2523f5a448a057ed2f68665e130482
smith97az-blip <smith97az@gmail.com>
2025-09-23 14:35:24 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCq21u_uqORV-psuTY2CfHvURqSEsvTH4Q",
   authDomain: "barn-strong-app.firebaseapp.com",
   projectId: "barn-strong-app",
-  storageBucket: "barn-strong-app.appspot.com",
+  storageBucket: "barn-strong-app.firebasestorage.app",
   messagingSenderId: "138604185630",
   appId: "1:138604185630:web:22768be8db4e4238f3c60f",
   measurementId: "G-D0LF645W7L"

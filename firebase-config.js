39ca24db7fe92c7a3026c8e5cda4a6359ab940e5
smith97az-blip <smith97az@gmail.com>
2025-09-23 14:37:29 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCq21u_uqORV-psuTY2CfHvURqSEsvTH4Q",
   authDomain: "barn-strong-app.firebaseapp.com",
@@ -9,4 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
